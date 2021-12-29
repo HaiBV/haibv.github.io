c46e5d43fc33d1eb408d4781ca26149b2a70b21a
HaiBV <buivuhai.bk@gmail.com>
2021-12-29 22:20:23 +0700

    Update gist: 125. Valid Palindrome

---
+++ gists/125.ValidPalindrome.js
@@ -0,0 +1,33 @@
+// 125. Valid Palindrome
+// Two points
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  if (s.length === 0) {
+    return true;
+  }
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start <= end) {
+    switch (true) {
+      case !/[a-zA-Z0-9]/.test(s[start]):
+        start++;
+        break;
+      case !/[a-zA-Z0-9]/.test(s[end]):
+        end--;
+        break;
+      case s[start].toLowerCase() !== s[end].toLowerCase():
+        return false;
+        break;
+      default:
+        start++;
+        end--;
+        break;
+    }
+  }
+
+  return true;
+};
