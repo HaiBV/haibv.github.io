d42ddfbdc94fbd04b84e56400ac0a7f365914ba3
HaiBV <buivuhai.bk@gmail.com>
2022-01-18 22:46:45 +0700

    Update gist: 13. Roman to Integer

---
+++ gists/leetcode/13.RomantoInteger.ts
@@ -0,0 +1,22 @@
+/**
+ * 13. Roman to Integer
+ * Hash Table, Math, String
+ */
+
+const symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  let value: number = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]] ? (value -= symbols[s[i]]) : (value += symbols[s[i]]);
+  }
+  return value;
+}
