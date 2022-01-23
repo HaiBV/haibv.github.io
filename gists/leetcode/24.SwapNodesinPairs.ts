e3449cc26c761775ea3d7c76ead416b6d5caaf66
HaiBV <buivuhai.bk@gmail.com>
2022-01-23 22:53:47 +0700

    Update gist: 24. Swap Nodes in Pairs

---
+++ gists/leetcode/24.SwapNodesinPairs.ts
@@ -0,0 +1,15 @@
+/**
+ * 24. Swap Nodes in Pairs
+ * Linked List, Recursion
+ */
+import ListNode from '../dataStructures/ListNode';
+
+function swapPairs(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  var v1 = head,
+    v2 = head.next,
+    v3 = v2.next;
+  v2.next = v1;
+  v1.next = swapPairs(v3);
+  return v2;
+}
