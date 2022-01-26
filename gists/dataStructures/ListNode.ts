881bff41e3d23d5a8c0289dbfbe888f40ec86c90
HaiBV <buivuhai.bk@gmail.com>
2022-01-26 22:36:44 +0700

    Update data structures: Linked List

--- gists/dataStructures/ListNode.ts
+++ gists/dataStructures/ListNode.ts
@@ -1,10 +1,70 @@
-class ListNode {
+export default class ListNode {
   val: number;
   next: ListNode | null;
+
   constructor(val?: number, next?: ListNode | null) {
     this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
 }
 
-export default ListNode
+export class LinkedList {
+  head: ListNode | null;
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let counter = 1;
+    let node = this.head;
+
+    while (node.next) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  push(val: number): void {
+    const node = new ListNode(val);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  unshift(val: number): void {
+    const node = new ListNode(val);
+
+    if (this.head) {
+      node.next = this.head;
+    }
+    this.head = node;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let cur: ListNode | null = this.head;
+    let res = [];
+    while (cur) {
+      res.push(cur.val);
+      cur = cur.next;
+    }
+
+    console.log(res.join(' -> '));
+  }
+}
