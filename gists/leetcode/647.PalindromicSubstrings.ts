3d551ac93747c3d4beaae06b9c86539cd49b46e3
HaiBV <buivuhai.bk@gmail.com>
2022-02-13 22:43:43 +0700

    Update gist: 647. Palindromic Substrings

---
+++ gists/leetcode/647.PalindromicSubstrings.ts
@@ -0,0 +1,28 @@
+/**
+ * 647. Palindromic Substrings
+ * String, Dynamic Programming
+ */
+
+function countSubstrings(s: string): number {
+  const l = s.length;
+  let counter = 0;
+
+  for (let i = 0; i < l; i++) {
+    let j = i - 1;
+    let k = i;
+    while (k < l - 1 && s[k] === s[k + 1]) {
+      k++;
+    }
+
+    counter += ((k - j) * (k - j + 1)) / 2;
+    i = k++;
+
+    while (j >= 0 && k < l && s[j] === s[k]) {
+      k++;
+      j--;
+      counter++;
+    }
+  }
+
+  return counter;
+}
