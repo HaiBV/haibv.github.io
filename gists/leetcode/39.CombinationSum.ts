830c085998a08be0d940cf70c166c49b8cf98dd8
HaiBV <buivuhai.bk@gmail.com>
2022-02-15 23:34:15 +0700

    Update gist: 39. Combination Sum

---
+++ gists/leetcode/39.CombinationSum.ts
@@ -0,0 +1,21 @@
+/**
+ * 39. Combination Sum
+ * Array, Backtracking
+ */
+
+function combinationSum(candidates: number[], target: number): number[][] {
+  var buffer = [];
+  var result = [];
+  search(0, target);
+  return result;
+
+  function search(startIdx: number, target: number) {
+    if (target === 0) return result.push(buffer.slice());
+    if (target < 0) return;
+    if (startIdx === candidates.length) return;
+    buffer.push(candidates[startIdx]);
+    search(startIdx, target - candidates[startIdx]);
+    buffer.pop();
+    search(startIdx + 1, target);
+  }
+}
--- gists/leetcode/70.ClimbingStairs.ts
+++ gists/leetcode/70.ClimbingStairs.ts
@@ -3,13 +3,28 @@
 // Dynamic Programming
 
 function climbStairs(n: number): number {
-    let steps: number[] = [];
-    steps[0] = 1;
-    steps[1] = 2;
-    
-    for (let i = 2; i < n; i++) {
-        steps[i] = steps[i - 2] + steps[i - 1];
-    }
-    
-    return steps[n - 1];
-};
+  let steps: number[] = [];
+  steps[0] = 1;
+  steps[1] = 2;
+
+  for (let i = 2; i < n; i++) {
+    steps[i] = steps[i - 2] + steps[i - 1];
+  }
+
+  return steps[n - 1];
+}
+
+function combineBuyer(numberBuyer: number): string[] {
+  let combines: string[][] = [];
+  combines[0] = [''];
+  combines[1] = ['1'];
+  combines[2] = ['1-1', '2'];
+
+  for (let i = 3; i <= numberBuyer; i++) {
+    combines[i] = [...combines[i - 1].map((combine) => `${combine}-1`), ...combines[i - 2].map((combine) => `${combine}-2`)];
+  }
+
+  return combines[numberBuyer];
+}
+
+// console.log(combineBuyer(4));
