141026dd3d852fa8710200c1fe062b269aefaf07
HaiBV <buivuhai.bk@gmail.com>
2022-02-21 22:01:48 +0700

    Update gist: 96. Unique Binary Search Trees

---
+++ gists/leetcode/96.UniqueBinarySearchTrees.js
@@ -0,0 +1,20 @@
+/**
+ * 96. Unique Binary Search Trees
+ * Math, Dynamic Programming, Binary Tree
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+  let G = new Array(n + 1).fill(0);
+  G[0] = 1;
+  G[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      G[i] += G[j - 1] * G[i - j];
+    }
+  }
+  return G[n];
+};
