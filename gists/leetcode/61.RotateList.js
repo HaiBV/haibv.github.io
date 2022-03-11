7a383c1aaab80deb0cb258f847e7139bd14e2b28
HaiBV <buivuhai.bk@gmail.com>
2022-03-11 23:07:41 +0700

    Update leetcode sulutions:61. Rotate List

---
+++ gists/leetcode/61.RotateList.js
@@ -0,0 +1,42 @@
+/**
+ * 61. Rotate List
+ * Linked List, Two Pointers
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function (head, k) {
+  if (!head) {
+    return head;
+  }
+
+  let len = 1; // number of nodes
+  let newHead = head;
+  let tail = head;
+
+  while (tail.next) {
+    // get the number of nodes in the list
+    tail = tail.next;
+    len++;
+  }
+  tail.next = head; // circle the link
+
+  if ((k %= len)) {
+    for (let i = 0; i < len - k; i++) {
+      tail = tail.next; // the tail node is the (len-k)-th node (1st node is head)
+    }
+  }
+  newHead = tail.next;
+  tail.next = null;
+  return newHead;
+};
