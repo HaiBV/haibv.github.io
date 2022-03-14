7dff8fd1b2300e5f8170c8438201570aaf52f9cf
HaiBV <buivuhai.bk@gmail.com>
2022-03-14 21:58:18 +0700

    Update leetcode sulution: 560. Subarray Sum Equals K

---
+++ gists/leetcode/560.SubarraySumEqualsK.js
@@ -0,0 +1,27 @@
+/**
+ * 560. Subarray Sum Equals K
+ * Array, Hash Table, Prefix Sum
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  let map = new Map();
+  let sum = 0;
+  let count = 0;
+  map.set(0, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum - k)) count += map.get(sum - k);
+    if (map.has(sum)) map.set(sum, map.get(sum) + 1);
+    else map.set(sum, 1);
+  }
+  return count;
+};
+
+subarraySum([1, 1, 1], 2); // 2
+
+subarraySum([1, 2, 3], 3); // 2
