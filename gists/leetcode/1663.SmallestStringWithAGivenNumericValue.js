4e5240b0f376d07e0b585643ede0919f701c844c
Hai Bui Vu <buivuhai.bk@gmail.com>
2022-03-22 17:57:31 +0700

    Update leetcode sulution: 1663. Smallest String With A Given Numeric Value

---
+++ gists/leetcode/1663.SmallestStringWithAGivenNumericValue.js
@@ -0,0 +1,35 @@
+/**
+ * 1663. Smallest String With A Given Numeric Value
+ * String, Greedy
+ */
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getSmallestString = function (n, k) {
+  let numericArr = [],
+    str = '';
+  // Step 1 to build string numeric array
+  for (let i = 1; i <= 26; i++) {
+    numericArr[i] = String.fromCharCode(96 + i);
+  }
+  for (let i = n; i > 0; i--) {
+    // Step 2: Find nth blanks value
+    let val = k - (i - 1);
+    // Step 3
+    if (val >= 26) {
+      str = numericArr[26] + str;
+      k = k - 26;
+    } else {
+      str = numericArr[val] + str;
+      k = k - val;
+    }
+  }
+  return str;
+};
+
+getSmallestString(3, 27); // "aay"
+
+getSmallestString(5, 73); // "aaszz"
