74a37ab0a1efb26a34423e7ee3fbae70988c6e29
HaiBV <buivuhai.bk@gmail.com>
2022-03-28 20:11:10 +0700

    Update leetcode sulution1: 81. Search in Rotated Sorted Array II

---
+++ gists/leetcode/81.SearchinRotatedSortedArray2.js
@@ -0,0 +1,33 @@
+/**
+ * 81. Search in Rotated Sorted Array II
+ * Array, Binary Search
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left < right) {
+    let mid = left + parseInt((right - left) / 2); // lower mid
+
+    if (nums[mid] == nums[right]) right--; // the only difference between Q.33
+    else if (nums[mid] < nums[right]) {
+      // right part sorted
+      if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+      else right = mid;
+    } else {
+      if (target > nums[mid] || target < nums[left]) left = mid + 1;
+      else right = mid;
+    }
+  }
+  return nums[left] == target;
+};
+
+search([2, 5, 6, 0, 0, 1, 2], 0); // true
+
+search([2, 5, 6, 0, 0, 1, 2], 3); // false
