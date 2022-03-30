4b366a8a9be8b7229f19fbbb620d52a734d8e50b
HaiBV <buivuhai.bk@gmail.com>
2022-03-30 20:47:24 +0700

    Update leetcode sulution1: 17. Letter Combinations of a Phone Number

---
+++ gists/leetcode/17.LetterCombinationsofaPhoneNumber.js
@@ -0,0 +1,43 @@
+/**
+ * 17. Letter Combinations of a Phone Number
+ * String, Hash Table, Backtracking
+ */
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = (digits) => {
+  if (digits == null || digits.length === 0) return [];
+
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const res = [];
+  const go = (i, s) => {
+    if (i === digits.length) {
+      res.push(s);
+      return;
+    }
+
+    for (const c of map[digits[i]]) {
+      go(i + 1, s + c);
+    }
+  };
+
+  go(0, '');
+  return res;
+};
+
+letterCombinations('23'); // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+letterCombinations(''); // []
+
+letterCombinations('2'); // ["a","b","c"]
