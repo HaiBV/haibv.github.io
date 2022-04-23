c9545ea895b9d86c0b0761b8dfe521ac2a26af2b
HaiBV <buivuhai.bk@gmail.com>
2022-04-23 23:07:10 +0700

    Leetcode sulution: 99. Recover Binary Search Tree

---
+++ gists/leetcode/535.EncodeandDecodeTinyURL.js
@@ -0,0 +1,41 @@
+/**
+ * 535. Encode and Decode TinyURL
+ * Hash Table, String
+ */
+
+let codeDB = new Map(),
+  urlDB = new Map();
+const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+const getCode = () => {
+  let code = new Array(6).fill().map((_) => chars.charAt(~~(Math.random() * 62)));
+  return 'http://tinyurl.com/' + code.join('');
+};
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+const encode = (longUrl) => {
+  if (urlDB.has(longUrl)) return urlDB.get(longUrl);
+  let code = getCode();
+  while (codeDB.has(code)) code = getCode();
+  codeDB.set(code, longUrl);
+  urlDB.set(longUrl, code);
+  return code;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+const decode = (shortUrl) => codeDB.get(shortUrl);
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
