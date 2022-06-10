7e6cd3bd520f7d3cde6ae0c16429bc1218f79a4d
HaiBV <buivuhai.bk@gmail.com>
2022-06-10 21:25:39 +0700

    Leetcode sulution: 3. Longest Substring Without Repeating Characters

---
+++ gists/leetcode/3.LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,24 @@
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * Hash Table, String, Sliding Window
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let max = 0;
+  let curMaxStr = "";
+
+  for (let c of s) {
+    if (curMaxStr.indexOf(c) > -1) {
+      curMaxStr = curMaxStr.substring(curMaxStr.indexOf(c) + 1);
+    }
+
+    curMaxStr += c;
+    max = Math.max(max, curMaxStr.length);
+  }
+
+  return max;
+};
